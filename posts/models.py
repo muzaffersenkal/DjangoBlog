6693fcbb7932af96a1538ab78a33a46c521e4127
muzaffersenkal <muzaffer727@hotmail.com>
2018-07-08 21:35:24 +0300

    İlk Modelimiz

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    created = models.DateField('date published')
