87a3c0b36f559a586c177e16d5db1ce503631e3a
muzaffersenkal <muzaffer727@hotmail.com>
2018-07-08 21:50:51 +0300

    Admin Panelinden Yazı Ekleme

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     created = models.DateField('date published')
+
+    def __str__(self):
+        return  self.title + " => " + str(self.created)
