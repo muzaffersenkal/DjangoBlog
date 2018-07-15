799bfe80d24e1106c27fa8046575a656741a6960
muzaffersenkal <muzaffer727@hotmail.com>
2018-07-15 18:40:03 +0300

    Tüm Gönderileri Listelemek

--- posts/urls.py
+++ posts/urls.py
@@ -21,6 +21,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:post_id>', views.single),
+    path('detail/<int:post_id>', views.single,name='single'),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -7,9 +7,9 @@ from .models import Post
 
 
 def index(request):
-    myObject  = Post.objects.all().first()
-    return render(request,'posts/index.html',{'post': myObject})
+    allPosts  = Post.objects.all()
+    return render(request,'posts/index.html',{'posts': allPosts})
 
 def single(request,post_id):
-
-    return render(request,'posts/single.html',{'single' : post_id })
+    post = get_object_or_404(Post,pk=post_id)
+    return render(request,'posts/single.html',{'single' : post })
