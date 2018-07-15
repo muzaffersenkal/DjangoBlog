45758ef546db1be6996933a5661786dfe36f1660
muzaffersenkal <muzaffer727@hotmail.com>
2018-07-15 14:21:55 +0300

    Template Render

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Merhaba Dünya!!")
+    myObject  = Post.objects.all().first()
+    return render(request,'posts/index.html',{'post': myObject})
