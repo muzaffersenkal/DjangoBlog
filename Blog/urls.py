900973e71d01dfa71c7539dcc269b0f2c29bdf89
muzaffersenkal <muzaffer727@hotmail.com>
2018-07-16 21:08:32 +0300

    static files

--- Blog/urls.py
+++ Blog/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls')),
-    path('',views.home),
+    path('', include('posts.urls')),
 
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
