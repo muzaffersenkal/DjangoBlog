5bbfcc1b73f07d8d389a261a3bc2c0870942a0e1
muzaffersenkal <muzaffer727@hotmail.com>
2018-07-29 15:46:01 +0300

    Yazıya resim ve kullanıcı ekleme

--- Blog/urls.py
+++ Blog/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('', include('posts.urls')),
 
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,default=1)
     title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='uploads/',blank=True)
     content = models.TextField()
     created = models.DateField('date published')
 
