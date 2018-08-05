36fa4aa4597280ee456a1dd5a646e43463dd90b9
muzaffersenkal <muzaffer727@hotmail.com>
2018-08-05 15:34:59 +0300

    Crispy Forms

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import Post
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class CreatePostForm(forms.ModelForm):
+
+    def __init__(self,*args,**kwargs):
+        super(CreatePostForm,self).__init__(*args,**kwargs)
+        self.helper = FormHelper()
+
+
+        self.helper.add_input(Submit('submit','Submit'))
+
 
 
     class Meta:
