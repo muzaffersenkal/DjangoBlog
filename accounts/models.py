d65eb056a867fdea90fe7158ffedd91bddfaa86e
muzaffersenkal <muzaffer727@hotmail.com>
2018-09-09 18:36:21 +0300

    Signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.conf import settings
 from django.template.defaultfilters import slugify
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -18,3 +20,11 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+
+post_save.connect(create_user_profile,sender=User)
