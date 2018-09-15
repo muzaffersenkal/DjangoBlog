a9e5e69766cdeb331532db95998d131775f24a03
muzaffersenkal <muzaffer727@hotmail.com>
2018-09-15 18:53:31 +0300

    Takip Et Özelliği için İlişki Kurma

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class UserProfile(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="profile")
 
     avatar = models.ImageField(upload_to="uploads/avatars/",default="uploads/avatars/default.jpg")
-
+    follow = models.ManyToManyField("self",blank=True,related_name="userFollow",symmetrical=False)
 
     slug = models.SlugField(unique=True,editable=False)
 
@@ -20,6 +20,16 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    #takip ettiği kişi sayısı
+
+    def followedCount(self):
+        return self.follow.count()
+
+    #takipçi sayısı
+
+    def followersCount(self):
+        return self.userFollow.all().count()
 
 def create_user_profile(sender,instance,created,**kwargs):
     if created:
